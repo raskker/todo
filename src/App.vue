b922f53db076c4a6030bba5a81c2c4d67e97ef4d
jdrabant <jakob-drabant>
2021-11-04 00:11:13 +0100

    waves ui elements added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
 <w-app>
 <h1>Todo App</h1>
-<w-button class="addTodoPlusIcon" @click="addTodoPressed = !addTodoPressed" :style="[addTodoPressed ? {'background-color':'red'} : {'background-color':'green'}]">{{addTodoPressed ? "Close" : "Add Todo"}}</w-button>
-<form v-if="addTodoPressed" @submit.prevent="addTodo">
-  <div class="name">
-    <label for="todoName">Todo Name: </label>
-    <input v-model="newTodoName" type="text" name="todoName" id="todoName"/>
-  </div>
-  <div class="desc">
-    <label for="todoDesc">Todo Beschreibung: </label>
-    <input v-model="newTodoDesc" type="text" name="todoDesc" id="todoDesc"/>
-  </div>
-  <button id="addTodo" type="submit">Todo speichern</button>
-</form>
+
+<w-button class="addTodoPlusIcon transition-toggle" @click="addTodoPressed = !addTodoPressed" :style="[addTodoPressed ? {'background-color':'red'} : {'background-color':'green'}]">{{addTodoPressed ? "Close" : "Add Todo"}}</w-button>
+
+<w-transition-fade>
+  <form v-if="addTodoPressed" @submit.prevent="addTodo">
+    <div class="name">
+      <w-input v-model="newTodoName" type="text" name="todoName" id="todoName">Todo Name:</w-input>
+    </div>
+    <div class="desc">
+      <w-input v-model="newTodoDesc" type="text" name="todoDesc" id="todoDesc">Todo Beschreibung:</w-input>
+    </div>
+    <w-button id="addTodo" type="submit">Todo speichern</w-button>
+  </form>
+</w-transition-fade>
+
 <div v-for="(todo, index) in todos" v-bind:key="todo.id">
   <div @click="toggleTodo(todo)" :class="{done: todo.done}">
+    <w-card tile>
+        <template #title>
+          <w-toolbar>
+            <div class="title2">{{todo.todoName}}</div>
+            <div class="spacer"></div>
+            <span @click="removeTodo(index)" class="mdi mdi-close ml2 removeTodo"></span>
+          </w-toolbar>
+        </template>
+        <p class="todoDesc">{{todo.todoDesc}}</p>
+    </w-card>
+  </div>
+  
+  <!-- <div @click="toggleTodo(todo)" :class="{done: todo.done}">
     <div>{{todo.todoName}}</div>
     <div>{{todo.todoDesc}}</div>
     <div @click="removeTodo(index)" class="removeTodo">X</div>
-  </div>
+  </div> -->
 </div>
 </w-app>
 </template>
@@ -77,5 +93,13 @@ body {
 
 .done {
   text-decoration: line-through;
+}
+
+.removeTodo {
+  cursor: pointer;
+}
+
+.todoDesc {
+  font-size: 0.5em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import WaveUI from 'wave-ui'
 import 'wave-ui/dist/wave-ui.css'
+import '@mdi/font/css/materialdesignicons.min.css'
 
 const app = createApp(App)
 
